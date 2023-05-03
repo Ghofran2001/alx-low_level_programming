849e9a515c173239b4f331399472237d31a0014f
Ghofran2001 <ghofran.kouach@gmail.com>
2023-05-03 15:10:26 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
